1311cc3efe2958c913aa7414f902d8a3258f1adb
Do Quang Tuan <tuank62ie1@gmail.com>
2020-10-20 15:06:48 +0700

    commit

--- My_DIM_WebApp/settings.py
+++ My_DIM_WebApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hx6a=@ojomufrxm%+-px^c_i=90lv&((%nm=&c)i=1=o-f8y_u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
